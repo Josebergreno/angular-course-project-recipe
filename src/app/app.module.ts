dabdb342c602dcb0373001a74770bb2d12980934
Joseph Bergren <bergren.bergren@yahoo.com>
2023-01-30 14:54:22 -0600

    structured project folders and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { ShoppingListComponent } from './shopping-list-feature/shopping-list/sho
 import { ShoppingListEditComponent } from './shopping-list-feature/shopping-list-edit/shopping-list-edit.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { HeaderComponent } from './header/header.component';
     ShoppingListComponent,
     RecipeBookComponent,
     HeaderComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
